b81c674f09f128e87b26bdb9d279263122c3c1b2
Colin Charleson <colin.charleson@ontariotechu.net>
2024-02-22 13:31:17 -0500

    Door mechanics

---
+++ EASE Demo/Assets/Scripts/DoorManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class DoorManager : MonoBehaviour
+{
+    [Header("Door Properties")]
+    [SerializeField] private float doorSpeed = 1;
+    [SerializeField] private float doorLift = 5;
+    private Vector3 endPos;
+
+    [Header("Conditions")]
+    [SerializeField] private Transform pushCube;
+    [SerializeField] private Transform targetLocation;
+    [SerializeField] private float cubeDistance = 0.5f;
+
+    public void Start() 
+    {
+        endPos = this.transform.position + Vector3.up * doorLift;
+    }
+
+    private void Update()
+    {
+        if (Vector3.Distance(pushCube.position, targetLocation.position) < cubeDistance) 
+            OpenDoor();
+    }
+    public void OpenDoor()
+    {
+        this.transform.position = Vector3.Lerp(this.transform.position, endPos, Time.deltaTime * doorSpeed);
+    }
+}
