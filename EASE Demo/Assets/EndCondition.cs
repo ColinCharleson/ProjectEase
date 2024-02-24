337a51cff0cb7c24b7928af2a4f4309e3edb9854
Colin Charleson <colin.charleson@ontariotechu.net>
2024-02-24 12:54:37 -0500

    End Condition

---
+++ EASE Demo/Assets/EndCondition.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+public class EndCondition : MonoBehaviour
+{
+    public GameObject endUI;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            endUI.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+}
